801f5cd9d8611f56d482d6029ca86413f3e8a3ad
jasonmiles <writejasonmiles@icloud.com>
2016-11-30 18:40:34 +0800

    Mission accomplished

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function(req, res) {
-  res.send("Hello World!")
+var monthNames = [
+                  "January",
+                  "February",
+                  "March",
+                  "April",
+                  "May",
+                  "June",
+                  "July",
+                  "August",
+                  "September",
+                  "October",
+                  "November",
+                  "December"
+                 ];
+
+app.get('/:date', function(req, res) {
+    var userDate = req.params.date
+    var unixTime = ""
+    var naturalTime = ""
+    if( (new Date(userDate*1000)).getTime()/1000|0 === userDate ) {
+      userDate = new Date(userDate*1000)
+      unixTime = (userDate.getTime()/1000|0).toString()
+      naturalTime = monthNames[userDate.getMonth()] + ' ' + userDate.getDate() + ', ' + userDate.getFullYear()
+    }
+    else if( Date.parse(userDate) ) {
+      userDate = new Date(userDate)
+      unixTime = (userDate.getTime()/1000|0).toString()
+      naturalTime = monthNames[userDate.getMonth()] + ' ' + userDate.getDate() + ', ' + userDate.getFullYear()
+    }
+    else {
+      unixTime = "null"
+      naturalTime = "null"
+    }
+
+    res.send({
+              "unix": unixTime,
+              "natural": naturalTime
+             })
+
 })
 
 app.listen(3000, function () {
