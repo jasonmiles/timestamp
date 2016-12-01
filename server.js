99d756f5a2f12ca398257635d2ed2ff6500cf1ad
jasonmiles <writejasonmiles@icloud.com>
2016-12-01 16:57:40 +0800

    heroku update

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.get('/:date', function(req, res) {
 
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000')
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port)
 })
